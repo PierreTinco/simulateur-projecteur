73c5b353724be4ed2b783b40e7f10c402b410508
Pierre <70861023+PierreTinco@users.noreply.github.com>
2023-08-20 18:03:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const WebSocket = require('ws');
-const port = 3001
+const port = 3002
 
 const wss = new WebSocket.Server({ port: port });
 
@@ -16,3 +16,14 @@ wss.on('connection', (ws) => {
 });
 
 console.log(`Server started on port ${port}`);
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(3001, () => {
+  console.log(`Example app listening on port ${port}`)
+})
